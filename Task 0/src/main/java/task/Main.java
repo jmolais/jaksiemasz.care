68b570cbcc8bea466a28233326e53f14a5cdf2c2
Josh Molais <josh.molais@outlook.com>
2017-12-06 17:31:20 +0100

    FINISHED TASK 0

--- Task 0/src/main/java/task/Main.java
+++ Task 0/src/main/java/task/Main.java
@@ -1,10 +1,23 @@
 package task;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        final int numberOfPeopleToCreate = 500;
+        List<Person> listOfRandomPeople = new ArrayList<Person>();
 
+        for (int i = 0; i < numberOfPeopleToCreate; i++) {
+            listOfRandomPeople.add(RandomPersonFactory.randomPerson());
+        }
+
+        Collections.sort(listOfRandomPeople);
+
+        for(Person person : listOfRandomPeople) {
+            person.printNicely();
+        }
     }
 }
-
